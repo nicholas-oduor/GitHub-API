459511297d50abfee6d84cdadc332499dd15741b
nicholas-oduor <oduor5742@gmail.com>
2021-02-06 14:19:59 +0300

    added ts file

---
+++ src/app/search-repositories/search-repositories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-repositories',
+  templateUrl: './search-repositories.component.html',
+  styleUrls: ['./search-repositories.component.css']
+})
+export class SearchRepositoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
