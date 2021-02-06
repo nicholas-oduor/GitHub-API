12f690ad51e1948c6b232ed343db38f704c91c4e
nicholas-oduor <oduor5742@gmail.com>
2021-02-06 13:23:40 +0300

    generated form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { GithubComponent } from './github/github.component';
+import { HomeRepoComponent } from './home-repo/home-repo.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MyAccountServicesComponent } from './my-account-services/my-account-services.component';
+import { MyReposComponent } from './my-repos/my-repos.component';
+import { NavComponent } from './nav/nav.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchRepositoriesComponent } from './search-repositories/search-repositories.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    GithubComponent,
+    HomeRepoComponent,
+    HomepageComponent,
+    MyAccountServicesComponent,
+    MyReposComponent,
+    NavComponent,
+    NotFoundComponent,
+    SearchRepositoriesComponent,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule,
