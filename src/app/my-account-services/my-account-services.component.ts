2340146940f6d59ec0fa203ea3d93a97ded63901
nicholas-oduor <oduor5742@gmail.com>
2021-02-06 13:41:53 +0300

    created ts in accnt-services

---
+++ src/app/my-account-services/my-account-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-account-services',
+  templateUrl: './my-account-services.component.html',
+  styleUrls: ['./my-account-services.component.css']
+})
+export class MyAccountServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
