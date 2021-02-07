e7a986bfafa0216cab354d69fd9d554f71704b51
nicholas-oduor <oduor5742@gmail.com>
2021-02-07 08:07:05 +0300

    updated ts homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from "../repo";
+import { User } from "../user";
+import { GitSearchService } from "../git-search.service";
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  repo!: Repo;
+  user!: User;
+  constructor(public gitSearchService:GitSearchService) { }
 
-  constructor() { }
+  getUsers(userName: any){ 
+    this.gitSearchService.getUsers(userName).then(
+      (success)=>{
+        this.user= this.gitSearchService.user;
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
+    this.gitSearchService.getRepo(userName).then(
+      (success)=>{
+        this.repo= this.gitSearchService.repo;
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
+  }
+  // getRepo(userName){ 
+    
+
+  // }
 
   ngOnInit(): void {
+    this.getUsers("nicholas-oduor");
   }
-
 }
