d8389ba2028c1fc3d17c09b9af8eb656419e6c7a
nicholas-oduor <oduor5742@gmail.com>
2021-02-07 07:59:17 +0300

    expoted  repo class & declared constructor public

---
+++ src/app/repo.ts
@@ -0,0 +1,9 @@
+export class Repo {
+    constructor(
+        public html_url:any,
+        public name:any,
+        public repos_url:any,
+        public description:any,
+        public forks_count:any,
+    ){}
+}
