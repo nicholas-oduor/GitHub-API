6cd9ed7fabe8bd638dd5d04bfd42fe7222309d3c
nicholas-oduor <oduor5742@gmail.com>
2021-02-07 07:55:04 +0300

    imported form & httpclient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms/';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import { GithubComponent } from './github/github.component';
-import { HomeRepoComponent } from './home-repo/home-repo.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { MyAccountServicesComponent } from './my-account-services/my-account-services.component';
-import { MyReposComponent } from './my-repos/my-repos.component';
 import { NavComponent } from './nav/nav.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { SearchRepositoriesComponent } from './search-repositories/search-repositories.component';
-import { SearchUserComponent } from './search-user/search-user.component';
+import { DatePipePipe } from './date-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    GithubComponent,
-    HomeRepoComponent,
     HomepageComponent,
-    MyAccountServicesComponent,
-    MyReposComponent,
     NavComponent,
     NotFoundComponent,
-    SearchRepositoriesComponent,
-    SearchUserComponent
+    DatePipePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
